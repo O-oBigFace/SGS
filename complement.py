48bc4fc394124f638063a21af1ef708c6d2eb80d
John <jiangh_wu@163.com>
2018-08-10 10:42:40 +0800

    first

---
+++ complement.py
@@ -0,0 +1,134 @@
+import os
+import openpyxl
+import scholar as scholarly
+import multiprocessing
+from SpiderKit.ip_pool_foreign import IPProvider
+import time
+import json
+import sys
+
+column = {
+    "expert": "A",
+    "affiliation": "B",
+    "interests": "C",
+    "email": "D",
+    "phone": "E",
+    "address": "F",
+    "country": "G",
+    "language": "H",
+    "position": "I",
+    "name": "J",
+    "citedby": "K",
+    "hindex": "L",
+    "hindex5y": "M",
+    "i10index": "N",
+    "i10index5y": "O",
+    "url_picture": "P",
+}
+
+path_excel = os.path.join(os.getcwd(), 'result.xlsx')
+path_recorder = os.path.join(os.getcwd(), 'record')
+path_result_file = os.path.join(os.getcwd(), 'result')
+
+# 读取文件
+reference = openpyxl.load_workbook(path_excel)
+sheet = reference.active
+with open(path_recorder, 'r', encoding='utf-8') as f:
+    # 记录结果字典：True - 结果已存在， False - 不存在
+    recoder_list = json.loads(f.read())
+
+
+def complement(lock, lower, upper, batch=5):
+    print(lower, "~", upper)
+    while lower < upper:
+        list_result = []
+        # 获取代理ip
+        ipprovider = IPProvider()
+        start = time.time()
+        proxy = ipprovider.get_ip()
+        _isIPNeedChange = False
+        for i in range(lower, min(lower + batch, upper + 1)):
+            if i > sheet.max_row:
+                break
+            if recoder_list[str(i)]:
+                print(i)
+                continue
+            if time.time() - start >= 60 or _isIPNeedChange:
+                start = time.time()
+                proxy = ipprovider.get_ip()
+                _isIPNeedChange = False
+            name = sheet[column["expert"] + str(i)].value
+            name = name if name is not None else ''
+
+            author = None
+            max_tries = 3
+            while author is None and max_tries > 0:
+                try:
+                    author = next(scholarly.search_author(name, proxy)).fill(proxy)
+                except StopIteration:
+                    print('No professor named', name, i)
+                    list_result.append((str(i), '', -1, -1, -1, -1, -1, '', ''))
+                    break
+                except Exception as e:
+                    _isIPNeedChange = True
+                    print(e, name, i)
+                    # 时间惩罚
+                    time.sleep(60)
+                    max_tries -= 1
+
+            if author is None:
+                list_result.append((str(i), '', -1, -1, -1, -1, -1, '', ''))
+                continue
+
+            name = author.name
+            affiliation = author.affiliation
+            citedby = author.citedby
+            hindex = author.hindex
+            hindex5y = author.hindex5y
+            i10index = author.i10index
+            i10index5y = author.i10index5y
+            url_picture = author.url_picture
+
+            result = (str(i), name, citedby, hindex, hindex5y, i10index, i10index5y, url_picture, affiliation)
+            list_result.append(result)
+            print(result)
+
+        lock.acquire()
+        try:
+            with open(path_result_file, 'a', encoding='utf-8') as file:
+                for r in list_result:
+                    file.write(json.dumps(r) + '\n')
+
+        finally:
+            lock.release()
+
+        lower = min(upper, lower + batch)
+
+
+if __name__ == '__main__':
+    with open(path_result_file, 'w', encoding='utf-8') as f:
+        pass
+
+    begin_no = sys.argv[1]
+    counts = sys.argv[2]
+    num_of_processing = 4 if len(sys.argv) < 4 else sys.argv[3]
+    have_done = 0
+
+    if begin_no + counts > sheet.max_row:
+        counts = sheet.max_row - begin_no + 1
+
+    quarter = counts // num_of_processing
+    lock = multiprocessing.Lock()
+    arg_list = [
+        (lock, have_done + begin_no, begin_no + quarter),
+        (lock, have_done + begin_no + quarter + 1, begin_no + quarter * 2),
+        (lock, have_done + begin_no + quarter * 2 + 1, begin_no + quarter * 3),
+        (lock, have_done + begin_no + quarter * 3 + 1, begin_no + quarter * 4),
+        (lock, have_done + begin_no + quarter * 4 + 1, begin_no + quarter * 5),
+        (lock, have_done + begin_no + quarter * 5 + 1, begin_no + quarter * 6),
+    ]
+
+    for j in range(1, num_of_processing + 1):
+        process = multiprocessing.Process(target=complement, args=arg_list[j - 1])
+        process.start()
+        time.sleep(30)
---
+++ recorder.py
@@ -0,0 +1,53 @@
+"""记录已爬取过的数据"""
+import os
+import openpyxl
+import json
+
+path_recorder = os.path.join(os.getcwd(), 'record')
+path_excel = os.path.join(os.getcwd(), 'result.xlsx')
+
+column = {
+    "expert": "A",
+    "affiliation": "B",
+    "interests": "C",
+    "email": "D",
+    "phone": "E",
+    "address": "F",
+    "country": "G",
+    "language": "H",
+    "position": "I",
+    "name": "J",
+    "citedby": "K",
+    "hindex": "L",
+    "hindex5y": "M",
+    "i10index": "N",
+    "i10index5y": "O",
+    "url_picture": "P",
+}
+
+
+def recorder(path):
+    record_dict = {}
+
+    wb = openpyxl.load_workbook(path)
+    sheet = wb.active
+
+    for i in range(2, sheet.max_row + 1):
+        if sheet[column["citedby"] + str(i)].value is not None:
+            record_dict[i] = True
+        else:
+            record_dict[i] = False
+
+    with open(path_recorder, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(record_dict))
+
+
+if __name__ == '__main__':
+    # recorder(path_excel)
+    with open(path_recorder, 'r', encoding='utf-8') as f:
+        dict_recorder = json.loads(f.read())
+        count = 0
+        for item in dict_recorder.values():
+            if item is True:
+                count += 1
+        print(count)
